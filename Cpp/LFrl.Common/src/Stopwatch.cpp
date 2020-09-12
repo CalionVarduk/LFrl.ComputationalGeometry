498b62869852db3962c269c266feb7c6e849087f
Łukasz Furlepa <lucas.furlepa@gmail.com>
2020-09-12 13:06:10 +0200

    Cpp -> LFrl.Common - Add Stopwatch class;

---
+++ Cpp/LFrl.Common/src/Stopwatch.cpp
@@ -0,0 +1,56 @@
+#include "stopwatch.h"
+
+using namespace LFRLCOMMON;
+
+StopWatch::StopWatch() noexcept
+	: _start(clock_type::now()), _end(), _isRunning(false) {
+	_end = _start;
+}
+
+StopWatch::StopWatch(bool start) noexcept
+	: _start(clock_type::now()), _end(), _isRunning(start) {
+	_end = _start;
+}
+
+bool StopWatch::IsRunning() const noexcept {
+	return _isRunning.load();
+}
+
+StopWatch::time_point_type StopWatch::GetStartPoint() const noexcept {
+	return _start;
+}
+
+StopWatch::time_point_type StopWatch::GetEndPoint() const noexcept {
+	return _isRunning ? clock_type::now() : _end;
+}
+
+StopWatch::duration_type StopWatch::GetElapsedTime() const {
+	return GetEndPoint() - _start;
+}
+
+u64 StopWatch::GetElapsedMs() const {
+	return std::chrono::duration_cast<std::chrono::duration<u64, std::milli>>(GetElapsedTime()).count();
+}
+
+u64 StopWatch::GetElapsedNs() const {
+	return std::chrono::duration<u64, std::nano>(GetElapsedTime()).count();
+}
+
+bool StopWatch::Start() noexcept {
+	if (_isRunning.exchange(true))
+		return false;
+	_start = _end = clock_type::now();
+	return true;
+}
+
+bool StopWatch::Stop() noexcept {
+	if (!_isRunning.exchange(false))
+		return false;
+	_end = clock_type::now();
+	return true;
+}
+
+void StopWatch::Restart() noexcept {
+	_isRunning.exchange(true);
+	_start = _end = clock_type::now();
+}
