4339d8b34aa565246e20e3570be7e8d7164c6a69
Łukasz Furlepa <lucas.furlepa@gmail.com>
2020-09-20 23:09:26 +0200

    Cpp -> OGL - Add TimerQuery primitive;

---
+++ Cpp/LFrl.OGL/src/primitives/TimerQuery.h
@@ -0,0 +1,59 @@
+#ifndef __LFRL_OGL_TIMER_QUERY_GUARD__
+#define __LFRL_OGL_TIMER_QUERY_GUARD__
+
+#include <chrono>
+#include "../ObjectState.h"
+
+BEGIN_LFRL_OGL_NAMESPACE
+
+struct TimerQuery final
+{
+	enum struct ActionResult
+	{
+		OK = 0,
+		ALREADY_INITIALIZED = 1,
+		QUERY_GEN_FAILURE = 2,
+		ALREADY_DISPOSED = 6,
+		NOT_READY = 7
+	};
+
+	TimerQuery(TimerQuery const&) = delete;
+	TimerQuery(TimerQuery&&) = default;
+	TimerQuery& operator=(TimerQuery const&) = delete;
+	TimerQuery& operator=(TimerQuery&&) = default;
+
+	TimerQuery() noexcept;
+	~TimerQuery() { Dispose(); }
+
+	GLuint GetActiveId() const noexcept { return _ids[_active]; }
+	GLuint GetInactiveId() const noexcept { return _ids[_inactive]; }
+	GLuint GetQueryCount() const noexcept { return _queryCount; }
+	GLuint64 GetLastElapsedTimeNs() const noexcept { return _lastElapsedTimeNs; }
+	GLuint64 GetTotalElapsedTimeNs() const noexcept { return _totalElapsedTimeNs; }
+	GLdouble GetAverageElapsedTimeNs() const noexcept { return _totalElapsedTimeNs / static_cast<GLdouble>(_queryCount); }
+	std::chrono::duration<GLuint64, std::chrono::nanoseconds::period> GetLastElapsedTime() const noexcept;
+	std::chrono::duration<GLuint64, std::chrono::nanoseconds::period> GetTotalElapsedTime() const noexcept;
+	std::chrono::duration<GLdouble, std::chrono::nanoseconds::period> GetAverageElapsedTime() const noexcept;
+	ObjectState GetState() const noexcept { return _state; }
+
+	ActionResult Initialize();
+
+	void Reset();
+	void Begin();
+	void End();
+
+	ActionResult Dispose();
+
+private:
+	GLuint _ids[2];
+	GLuint _active;
+	GLuint _inactive;
+	GLuint _queryCount;
+	GLuint64 _lastElapsedTimeNs;
+	GLuint64 _totalElapsedTimeNs;
+	ObjectState _state;
+};
+
+END_LFRL_OGL_NAMESPACE
+
+#endif
